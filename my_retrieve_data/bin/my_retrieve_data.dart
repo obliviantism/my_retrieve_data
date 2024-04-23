df59528f986e7b35a3dac17959d2995d9fd78d91
obliviantism <113970186+obliviantism@users.noreply.github.com>
2024-04-23 09:19:44 +0000

    1

---
+++ my_retrieve_data/bin/my_retrieve_data.dart
@@ -0,0 +1,5 @@
+import 'package:my_retrieve_data/my_retrieve_data.dart' as my_retrieve_data;
+
+void main(List<String> arguments) {
+  print('Hello world: ${my_retrieve_data.calculate()}!');
+}
---
+++ my_retrieve_data/test/my_retrieve_data_test.dart
@@ -0,0 +1,8 @@
+import 'package:my_retrieve_data/my_retrieve_data.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
